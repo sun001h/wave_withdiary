fb7ee6463efe8396702e3f9c5cab39698b1f15f8
susie <susie@DESKTOP-IMSQ1D6>
2021-06-01 13:17:10 +0900

    글쓰기 UI파일 넣음 (박다희)

--- wave.with.diary/src/main/java/com/wave/withdiary/board/BoardController.java
+++ wave.with.diary/src/main/java/com/wave/withdiary/board/BoardController.java
@@ -39,12 +39,32 @@ public class BoardController {
 	
 	
 	@RequestMapping(value = "/board/writeForm", method = RequestMethod.GET)
-	public String writeForm(Model model, BoardVO vo) {
+	public String writeForm(Model model, HttpServletRequest request, HttpServletResponse response) {
 		
 		logger.info("글 작성 화면");
+		
+		HttpSession session = request.getSession();
+		MemberVO vo = (MemberVO) session.getAttribute("member");
+		String memberCode = vo.getMemberCode();
+		model.addAttribute("vo", vo);
+		// 특정 멤버코드의 친구들을 조회함
+		List<String> friendList = friendService.friend(memberCode);
+		System.out.println(friendList);
+		System.out.println(friendList.size());
+		
+		// 그러고 나서 그 멤버코드들로 리스트를 불러옴
+		List<MemberVO> friendVOList = new ArrayList<MemberVO>();
+		for(int i=0; i<friendList.size(); i++) {
+			MemberVO friend = new MemberVO();
+			String friendCode = friendList.get(i);
+			friend = service.selectMember(friendCode);
+			friendVOList.add(i, friend);
+		}
+		
+		model.addAttribute("friendList", friendVOList);
+		
 
-
-		return "board_writeForm";
+		return "board_writeForm2";
 	}
 
 	@RequestMapping(value="/board/insert", method = RequestMethod.POST)
