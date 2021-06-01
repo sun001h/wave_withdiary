9b37c39c2a28dcd51d00536563d47c53fdfec991
HKEDU <HKEDU@DESKTOP-E8LSTNN>
2021-06-01 14:42:46 +0900

    board_view2 수정

--- wave.with.diary/src/main/java/com/wave/withdiary/board/BoardController.java
+++ wave.with.diary/src/main/java/com/wave/withdiary/board/BoardController.java
@@ -82,14 +82,35 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/board/view", method = RequestMethod.GET)
-	public String view(Model model, int d_articlenumber, String clear) {
+	public String view(Model model, int d_articlenumber, String clear, HttpServletRequest request, HttpServletResponse response) {
 		
 		logger.info("글 상세보기");
 
-		BoardVO vo = boardService.read(d_articlenumber);
+		HttpSession session = request.getSession();
+		MemberVO vo = (MemberVO) session.getAttribute("member");
+		String memberCode = vo.getMemberCode();
 		model.addAttribute("vo", vo);
+		// 특정 멤버코드의 친구들을 조회함
+		List<String> friendList = friendService.friend(memberCode);
+		System.out.println(friendList);
+		System.out.println(friendList.size());
+		
+		// 그러고 나서 그 멤버코드들로 리스트를 불러옴
+		List<MemberVO> friendVOList = new ArrayList<MemberVO>();
+		for(int i=0; i<friendList.size(); i++) {
+			MemberVO friend = new MemberVO();
+			String friendCode = friendList.get(i);
+			friend = service.selectMember(friendCode);
+			friendVOList.add(i, friend);
+		}
+		
+		model.addAttribute("friendList", friendVOList);
+		
+		
+		BoardVO boardvo = boardService.read(d_articlenumber);
+		model.addAttribute("boardvo", boardvo);
 
-		return "board_view";
+		return "board_view2";
 	}	
 	
 	@RequestMapping(value = "/board/updateForm", method = RequestMethod.GET)
